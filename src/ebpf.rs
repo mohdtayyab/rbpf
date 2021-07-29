1b28f9bbd8b2f6b37b0eaef0aa0dc0270ef922ea
Mohd Tayyab <ta8041936@gmail.com>
2021-07-29 16:25:18 +0530

    Update ebpf.rs

--- src/ebpf.rs
+++ src/ebpf.rs
@@ -441,7 +441,7 @@ impl Insn {
     /// # Examples
     ///
     /// ```
-    /// use solana_rbpf::ebpf;
+    /// use giit_rbpf::ebpf;
     ///
     /// let prog: &[u8] = &[
     ///     0xb7, 0x12, 0x56, 0x34, 0xde, 0xbc, 0x9a, 0x78,
@@ -474,7 +474,7 @@ impl Insn {
     /// # Examples
     ///
     /// ```
-    /// use solana_rbpf::ebpf;
+    /// use giit_rbpf::ebpf;
     ///
     /// let prog: Vec<u8> = vec![
     ///     0xb7, 0x12, 0x56, 0x34, 0xde, 0xbc, 0x9a, 0x78,
@@ -505,7 +505,7 @@ impl Insn {
 /// # Examples
 ///
 /// ```
-/// use solana_rbpf::ebpf;
+/// use giit_rbpf::ebpf;
 ///
 /// let prog = &[
 ///     0xb7, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -518,7 +518,7 @@ impl Insn {
 /// The example below will panic, since the last instruction is not complete and cannot be loaded.
 ///
 /// ```rust,should_panic
-/// use solana_rbpf::ebpf;
+/// use giit_rbpf::ebpf;
 ///
 /// let prog = &[
 ///     0xb7, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
