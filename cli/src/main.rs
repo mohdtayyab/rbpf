95ce0a93c94b2fba033e61910a8cd32dbf3ef3d8
Mohd Tayyab <ta8041936@gmail.com>
2021-08-02 17:21:19 +0530

    Update main.rs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -35,7 +35,7 @@ impl SyscallObject<UserError> for MockSyscall {
 
 fn main() {
     let matches = App::new("Solana RBPF CLI")
-        .version("0.2.14")
+        .version("0.2.20")
         .author("Solana Maintainers <maintainers@solana.foundation>")
         .about("CLI to test and analyze eBPF programs")
         .arg(
